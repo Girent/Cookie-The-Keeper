2f1e3b7c9d26498bbc390eac357956dbb8f43b98
Girent <98615658+Girent@users.noreply.github.com>
2022-07-21 13:20:45 +0300

    Fix Mono to Network behaviour

--- Assets/GameScripts/PlayerProperties.cs
+++ Assets/GameScripts/PlayerProperties.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerProperties : MonoBehaviour
+public class PlayerProperties : NetworkBehaviour
 {
     [SerializeField] private int maxPlayerHealth;
     [SerializeField] private int playerDamage;
