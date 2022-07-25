5834e46d66bea373ca7b556c3786221c58789bfa
andreyvoinn-mdls <voinn.andrey@gmail.com>
2022-07-25 19:25:51 +0200

    Separate classes for association

---
+++ Assets/GameScripts/Player/Characteristics/Damage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+
+public class Damage : MonoBehaviour, IProperty
+{
+    public const float MaxDamage = 100;
+    public const float MinDamage = 0;
+    public float Amount { get; set;}
+    private float amount;
+
+    public void Decrease(int amount)
+    {
+        if (Amount - amount < MinDamage)
+            throw new InvalidOperationException();
+       
+        Amount -= amount;
+
+    }
+
+    public void Increase(int amount)
+    {
+        if (Amount + amount > MaxDamage)
+            throw new InvalidOperationException();
+
+        Amount += amount;
+    }
+}
+
---
+++ Assets/GameScripts/Player/Characteristics/Wood.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using TMPro;
+
+public class Wood : MonoBehaviour, IProperty
+{
+    [SerializeField] private TextMeshProUGUI counterUI;
+    
+    public Wood()
+    {
+        Amout = 0;
+    }
+
+    public const int MinAmount = 0;
+    public int Amout
+    {
+        get 
+        { 
+            return amout; 
+        }
+        private set
+        {
+            amout = value;
+            counterUI.text = amout.ToString();
+        }        
+    }
+    private int amout;
+
+    public void Increase(int amount)
+    {
+        Amout += amount;
+    }
+
+    public void Decrease(int amount)
+    {
+        if (Amout - amount < MinAmount)
+            throw new InvalidOperationException();
+
+        Amout -= amount;
+
+    }
+}
+
