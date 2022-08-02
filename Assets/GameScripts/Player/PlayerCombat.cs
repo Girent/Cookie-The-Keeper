8d3755799994e18d51d21823554c8d127c02265f
Girent <98615658+Girent@users.noreply.github.com>
2022-08-02 10:17:04 +0300

    AddAttackAnim

--- Assets/GameScripts/Player/PlayerCombat.cs
+++ Assets/GameScripts/Player/PlayerCombat.cs
@@ -3,16 +3,18 @@ using UnityEngine;
 
 public class PlayerCombat : NetworkBehaviour
 {
+    [SerializeField] private float attackRange;
     [SerializeField] private Transform attackPoint;
     [SerializeField] private LayerMask enemyLayers;
     [SerializeField] UIJoystick joystickInput;
-    [SerializeField] private float attackRange;
 
+    private NetworkAnimator networkAnimator;
     private Damage damage;
 
     private void Awake()
     {
         damage = GetComponent<Damage>();
+        networkAnimator = GetComponent<NetworkAnimator>();
     }
 
     public void Attack ()
@@ -20,6 +22,9 @@ public class PlayerCombat : NetworkBehaviour
         attackPoint.localPosition = joystickInput.GetLastDirection() * attackRange;
 
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange / 2f, enemyLayers);
+
+        networkAnimator.animator.SetFloat("AttackVector", joystickInput.GetLastDirection().x);
+        networkAnimator.SetTrigger("Attack");
 
         foreach (var enemy in hitEnemies)
         {
